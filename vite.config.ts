2e869be6f0b60d96ef48bc9feac35c9630a6086c
Mark Stover <stovenator@gmail.com>
2024-11-13 11:35:26 -0600

    Run prettier on everything

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 
 export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        plugins: [react()],
-        server: {
-            open: true,
-            port: parseInt(env.PORT) || 4200,
-        },
-        preview:{
-            open: true,
-            port: parseInt(env.PORT) || 4200,
-        }
-    };
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+      port: parseInt(env.PORT) || 4200,
+    },
+    preview: {
+      open: true,
+      port: parseInt(env.PORT) || 4200,
+    },
+  };
 });
